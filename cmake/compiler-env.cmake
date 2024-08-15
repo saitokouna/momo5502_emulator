a154eb3ec9c896674d97ab6d410505495a07518e
momo5502 <mauriceheumann@gmail.com>
2024-08-15 19:00:01 +0200

    Initial commit

---
+++ cmake/compiler-env.cmake
@@ -0,0 +1,147 @@
+include_guard()
+
+##########################################
+# System identification
+
+set(OSX OFF)
+set(LINUX OFF)
+set(WIN OFF)
+
+if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+    set(LINUX ON)
+elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+    set(OSX ON)
+elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
+    set(WIN ON)
+endif()
+
+##########################################
+
+cmake_policy(SET CMP0069 NEW) 
+set(CMAKE_POLICY_DEFAULT_CMP0069 NEW)
+
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+#set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
+
+##########################################
+
+if(UNIX)
+  momo_add_c_and_cxx_compile_options(-fvisibility=hidden)
+endif()
+
+##########################################
+
+if(LINUX)
+  add_link_options(
+    -Wl,--no-undefined
+    -Wl,--gc-sections
+    -Wl,-z,now
+    -Wl,-z,noexecstack
+    -static-libstdc++
+  )
+
+  momo_add_c_and_cxx_compile_options(
+    -ffunction-sections
+    -fdata-sections
+    -fstack-protector-strong
+    -fdiagnostics-color=always
+  )
+
+  add_compile_definitions(
+    _REENTRANT
+    _THREAD_SAFE
+  )
+
+  if(NOT MOMO_ENABLE_SANITIZER)
+    add_compile_definitions(
+      _FORTIFY_SOURCE=2
+    )
+  endif()
+
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pie")
+endif()
+
+##########################################
+
+if(MSVC)
+  string(REPLACE "/EHsc" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  string(REPLACE "/EHs" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+
+  momo_add_c_and_cxx_compile_options(
+    /sdl
+    /GS
+    /Gy
+    /EHa
+    /guard:cf
+  )
+
+  momo_add_compile_options(CXX
+    /Zc:__cplusplus
+  )
+
+  add_link_options(
+    /INCREMENTAL:NO
+  )
+
+  momo_add_c_and_cxx_release_compile_options(
+    /Ob2
+    #/GL
+  )
+
+  momo_add_release_link_options(
+    #/LTCG
+  )
+endif()
+
+##########################################
+
+if(MOMO_ENABLE_SANITIZER)
+momo_add_c_and_cxx_compile_options(
+  -fsanitize=address
+)
+endif()
+
+##########################################
+
+if(MOMO_ENABLE_SANITIZER)
+  # ASAN on Windows needs /MD
+  # https://developercommunity.visualstudio.com/t/c-address-sanitizer-statically-linked-dlls-do-not/1403680
+  set(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>DLL)
+else()
+  set(CMAKE_MSVC_RUNTIME_LIBRARY MultiThreaded$<$<CONFIG:Debug>:Debug>)
+endif()
+
+##########################################
+
+if(MSVC)
+  add_link_options(
+    $<$<NOT:$<STREQUAL:${CMAKE_MSVC_RUNTIME_LIBRARY},MultiThreaded>>:/NODEFAULTLIB:libcmt.lib>
+    $<$<NOT:$<STREQUAL:${CMAKE_MSVC_RUNTIME_LIBRARY},MultiThreadedDLL>>:/NODEFAULTLIB:msvcrt.lib>
+    $<$<NOT:$<STREQUAL:${CMAKE_MSVC_RUNTIME_LIBRARY},MultiThreadedDebug>>:/NODEFAULTLIB:libcmtd.lib>
+    $<$<NOT:$<STREQUAL:${CMAKE_MSVC_RUNTIME_LIBRARY},MultiThreadedDebugDLL>>:/NODEFAULTLIB:msvcrtd.lib>
+  )
+endif()
+
+##########################################
+
+set(OPT_DEBUG "-O0 -g")
+set(OPT_RELEASE "-O3 -g")
+
+if(MSVC)
+  set(OPT_DEBUG "/Od /Ob0 /Zi")
+  set(OPT_RELEASE "/O2 /Ob2 /Zi")
+
+  add_link_options(/DEBUG)
+endif()
+
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${OPT_DEBUG}")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${OPT_DEBUG}")
+
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${OPT_RELEASE}")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${OPT_RELEASE}")
+
+##########################################
+
+if(CMAKE_GENERATOR MATCHES "Visual Studio")
+  momo_add_c_and_cxx_compile_options(/MP)
+endif()
---
+++ src/emulator/main.cpp
@@ -0,0 +1,84 @@
+#include "std_include.hpp"
+
+// code to be emulated
+#define X86_CODE32 "\x41\x4a" // INC ecx; DEC edx
+
+// memory address where emulation starts
+#define ADDRESS 0x1000000
+
+
+namespace
+{
+	void run()
+	{
+		uc_engine* uc;
+		uc_err err;
+		int r_ecx = 0x1234;     // ECX register
+		int r_edx = 0x7890;     // EDX register
+
+		printf("Emulate i386 code\n");
+
+		// Initialize emulator in X86-32bit mode
+		err = uc_open(UC_ARCH_X86, UC_MODE_32, &uc);
+		if (err != UC_ERR_OK) {
+			printf("Failed on uc_open() with error returned: %u\n", err);
+			return ;
+		}
+
+		// map 2MB memory for this emulation
+		uc_mem_map(uc, ADDRESS, 0x1000, UC_PROT_ALL);
+
+		// write machine code to be emulated to memory
+		if (uc_mem_write(uc, ADDRESS, X86_CODE32, sizeof(X86_CODE32) - 1)) {
+			printf("Failed to write emulation code to memory, quit!\n");
+			return;
+		}
+
+		// initialize machine registers
+		uc_reg_write(uc, UC_X86_REG_ECX, &r_ecx);
+		uc_reg_write(uc, UC_X86_REG_EDX, &r_edx);
+
+		// emulate code in infinite time & unlimited instructions
+		err = uc_emu_start(uc, ADDRESS, ADDRESS + sizeof(X86_CODE32) - 1, 0, 0);
+		if (err) {
+			printf("Failed on uc_emu_start() with error returned %u: %s\n",
+				err, uc_strerror(err));
+		}
+
+		// now print out some registers
+		printf("Emulation done. Below is the CPU context\n");
+
+		uc_reg_read(uc, UC_X86_REG_ECX, &r_ecx);
+		uc_reg_read(uc, UC_X86_REG_EDX, &r_edx);
+		printf(">>> ECX = 0x%x\n", r_ecx);
+		printf(">>> EDX = 0x%x\n", r_edx);
+
+		uc_close(uc);
+	}
+}
+
+int main(int /*argc*/, char** /*argv*/)
+{
+	try
+	{
+		run();
+		return 0;
+	}
+	catch (std::exception& e)
+	{
+		puts(e.what());
+
+#ifdef _WIN32
+		MessageBoxA(nullptr, e.what(), "ERROR", MB_ICONERROR);
+#endif
+	}
+
+	return 1;
+}
+
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR, int)
+{
+	return main(__argc, __argv);
+}
+#endif

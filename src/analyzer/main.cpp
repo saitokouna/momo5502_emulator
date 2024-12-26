42b35047caf76536a8f81650fd4af3460cc3a86b
momo5502 <mauriceheumann@gmail.com>
2024-12-26 09:43:55 +0100

    Reduce output

--- src/analyzer/main.cpp
+++ src/analyzer/main.cpp
@@ -98,6 +98,7 @@ namespace
 		emulator_settings settings{
 			.application = argv[0],
 			.arguments = parse_arguments(argv, argc),
+			.silent_until_main = true,
 		};
 
 		windows_emulator win_emu{std::move(settings)};

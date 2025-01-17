3ecfa40ea738ca8265bafc55bb3d17d9591961d5
momo5502 <mauriceheumann@gmail.com>
2025-01-17 19:52:26 +0100

    Remove comments

--- deps/zlib.cmake
+++ deps/zlib.cmake
@@ -4,9 +4,6 @@ target_compile_definitions(zlibstatic PUBLIC ZLIB_CONST=1)
 target_include_directories(zlibstatic PUBLIC ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
 
 if (TARGET zlib)
-    # Remove all properties and dependencies from the zlib shared lib target
-    # (this will keep the zlibstatic target)
     set_target_properties(zlib PROPERTIES EXCLUDE_FROM_ALL TRUE)
     set_target_properties(zlib PROPERTIES EXCLUDE_FROM_DEFAULT_BUILD TRUE)
-    message(STATUS "Target 'zlib' has been disabled.")
 endif()

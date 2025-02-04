a5b330632daf2a158a8ea900b236a4582ba788b7
Maurice Heumann <mauriceheumann@gmail.com>
2025-02-04 09:49:51 +0100

    Update zlib

--- deps/zlib.cmake
+++ deps/zlib.cmake
@@ -1,7 +1,11 @@
-set(ZLIB_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(ZLIB_BUILD_TESTING OFF CACHE BOOL "" FORCE)
+set(ZLIB_BUILD_SHARED OFF CACHE BOOL "" FORCE)
+set(ZLIB_BUILD_MINIZIP OFF CACHE BOOL "" FORCE)
+set(ZLIB_INSTALL OFF CACHE BOOL "" FORCE)
+
 add_subdirectory(zlib)
 target_compile_definitions(zlibstatic PUBLIC ZLIB_CONST=1)
-target_include_directories(zlibstatic PUBLIC ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
+#target_include_directories(zlibstatic PUBLIC ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
 
 if (TARGET zlib)
     set_target_properties(zlib PROPERTIES EXCLUDE_FROM_ALL TRUE)
